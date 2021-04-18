8aa79a36255ff3111b43ac056b38ed5d704335c6
aleangelozi <aleangelozi@gmail.com>
2021-04-17 21:32:16 -0300

    Initial commit

---
+++ src/0_variables.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    // Mutable variables can be reassigned.
+    var age = 30
+
+    println(age) // It prints the current value of the variable.
+
+    age = 45 // It reassigns the variable's value
+
+    println(age) // It prints the current value of the variable.
+
+    // Read-only variables can't be reassigned.
+    val name = "Alessandro"
+
+    println(name) // It prints the current value of the variable.
+
+}
